657279c2f0183abe740aa790fe6627d6650b090c
lance-solve <lance@solvedata.io>
2025-02-10 21:00:40 +1100

    change routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Restaurants from "./Pages/Restaurants.js";
 import Home from "./Pages/Home.js";
 
